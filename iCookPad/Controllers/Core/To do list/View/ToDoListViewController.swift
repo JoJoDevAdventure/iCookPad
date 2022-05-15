8ebceaa6f86fb013054b12dc3e7e9cdb3d932171
Youssef <youssef.b.air2@gmail.com>
2022-05-15 22:53:45 +0100

    + To do list view files

---
+++ iCookPad/Controllers/Core/To do list/View/ToDoListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ToDoListViewController.swift
+//  iCookPad
+//
+//  Created by Youssef Bhl on 15/05/2022.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - View Model
+    let viewModel: ToDoListViewModel
+    
+    init(viewModel: ToDoListViewModel) {
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Set up
+    
+    
+    // MARK: - Functions
+    
+    
+    // MARK: - Network Manager calls
+    
+    
+}
+// MARK: - Extensions
---
+++ iCookPad/Controllers/Core/To do list/ViewModel/ToDoListViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoListViewModel.swift
+//  iCookPad
+//
+//  Created by Youssef Bhl on 15/05/2022.
+//
+
+import Foundation
+
+class ToDoListViewModel {
+    
+    init() {
+        
+    }
+    
+}
