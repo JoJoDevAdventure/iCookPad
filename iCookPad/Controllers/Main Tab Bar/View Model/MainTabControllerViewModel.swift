b258480ce1f957bd43b2ae300a94b9e42a2c0866
Youssef <youssef.b.air2@gmail.com>
2022-05-15 22:26:19 +0100

    + main tab bar files

---
+++ iCookPad/Controllers/Main Tab Bar/View Model/MainTabControllerViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabControllerViewModel.swift
+//  iCookPad
+//
+//  Created by Youssef Bhl on 15/05/2022.
+//
+
+import Foundation
+
---
+++ iCookPad/Controllers/Main Tab Bar/View/MainTabController.swift
@@ -0,0 +1,12 @@
+//
+//  MainTabController.swift
+//  iCookPad
+//
+//  Created by Youssef Bhl on 15/05/2022.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+}
