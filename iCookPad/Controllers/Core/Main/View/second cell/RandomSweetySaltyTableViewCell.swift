8975f3577642312c343afeb5e3f56528d1f18351
Youssef <youssef.b.air2@gmail.com>
2022-05-20 22:34:59 +0100

    second cell files

---
+++ iCookPad/Controllers/Core/Main/View/second cell/RandomSweetySaltyTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  RandomSweetySaltyTableViewCell.swift
+//  iCookPad
+//
+//  Created by Youssef Bhl on 20/05/2022.
+//
+
+import UIKit
+
+class RandomSweetySaltyTableViewCell: UITableViewCell {
+
+    
+
+}
