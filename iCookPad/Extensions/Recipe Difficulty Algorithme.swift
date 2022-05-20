89b20ad4c5f0443ab5215a3da83a79f79eb74e6f
Youssef <youssef.b.air2@gmail.com>
2022-05-20 16:09:21 +0100

    difficulty ranges

--- iCookPad/Extensions/Recipe Difficulty Algorithme.swift
+++ iCookPad/Extensions/Recipe Difficulty Algorithme.swift
@@ -9,16 +9,26 @@ import Foundation
 
 struct DifficultyProperty {
     func calculateDifficulty(recipe: Recipe) -> Int {
+        let steps = Double(recipe.extendedIngredients.count)
+        let prepTime = Double(recipe.readyInMinutes)
+        let coast = recipe.pricePerServing
         //difficulty calcul :
         /*
          property in calcul : number of steps, coast, prep time
          return should be 1 / 2 / 3 / 4 / 5 / 6
-         1 : low steps (2) / coast (7$) / preptime(5min)
-         6 : mac steps (16) / coast (400$) / preptime (120 min)
-         
+         1 : low steps (2) / coast (7$) / preptime(5min) = 5
+         6 : mac steps (16) / coast (400$) / preptime (120 min) = 50
          mesure = step coast per minute
          (coast * step) / min
+         
+         1//       < 13.3
+         2// 13.3 -> 21.6
+         3// 21.6 -> 29.9
+         4// 29.9 -> 38.2
+         5// 38.2 -> 46.5
+         6// 46.5 >
          */
+        let setupCoastPerMinute = (steps * coast) / prepTime
         return 0
     }
 }
