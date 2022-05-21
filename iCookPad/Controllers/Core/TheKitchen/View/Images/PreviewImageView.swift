43d0285f3767581c020b9cbe198e161cf9c293d3
Youssef <youssef.b.air2@gmail.com>
2022-05-21 03:44:55 +0100

    preview Image fix

--- iCookPad/Controllers/Core/TheKitchen/View/Images/PreviewImageView.swift
+++ iCookPad/Controllers/Core/TheKitchen/View/Images/PreviewImageView.swift
@@ -12,13 +12,16 @@ class PreviewImageView: UIImageView {
     func configure() {
         
         layer.cornerRadius = 30
+        contentMode = .scaleAspectFill
         clipsToBounds = true
+        layer.borderWidth = 0.2
+        layer.borderColor = UIColor.lightGray.cgColor
         backgroundColor = .lightGray
         layer.shadowColor = UIColor.black.cgColor
-        layer.shadowOffset = CGSize(width: 3,
-                                          height: 3)
-        layer.shadowRadius = 2
-        layer.shadowOpacity = 0.4
+        layer.shadowOffset = CGSize(width: 4,
+                                          height: 4)
+        layer.shadowRadius = 6
+        layer.shadowOpacity = 0.3
         translatesAutoresizingMaskIntoConstraints = false
     }
 
