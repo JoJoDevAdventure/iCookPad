dec2d139b4a97d15aee1cb5306e6cfd5d8154015
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-06-02 23:43:33 +0100

    Details recipe files #1

---
+++ iCookPad/Controllers/Detail Recipe/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  iCookPad
+//
+//  Created by Youssef Bhl on 02/06/2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
