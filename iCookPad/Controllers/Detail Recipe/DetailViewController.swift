21fa7d939ba06ed6352fc75dc13a7daef4290b7a
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-06-07 22:02:24 +0100

    + Scroll view

--- iCookPad/Controllers/Detail Recipe/DetailViewController.swift
+++ iCookPad/Controllers/Detail Recipe/DetailViewController.swift
@@ -12,12 +12,24 @@ class DetailViewController: UIViewController {
     // MARK: - Properties
     
     // MARK: - UI
+    //scroll view
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.showsVerticalScrollIndicator = false
+        return scrollView
+    }()
+    
     //Title
     private lazy var titleLabel: TitleLabel = {
         let title = TitleLabel()
         title.configure(fontSize: 42)
         title.text = "Test Recipe Title"
         return title
+    }()
+    
+    private lazy var previewImage: PreviewImageView {
+        let image 
     }()
     
     // MARK: - View Model
