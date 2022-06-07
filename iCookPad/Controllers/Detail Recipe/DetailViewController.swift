f88ba85a1ce92eb97588bd45eb12df41ed3956b7
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-06-07 19:53:44 +0100

    Code structure

--- iCookPad/Controllers/Detail Recipe/DetailViewController.swift
+++ iCookPad/Controllers/Detail Recipe/DetailViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    // MARK: - UI
+    
+    // MARK: - View Model
+    
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    
+    // MARK: - Set up
+    
+    
+    // MARK: - Functions
+    
+    
+    
+    // MARK: - Extensions
     
     
 }
