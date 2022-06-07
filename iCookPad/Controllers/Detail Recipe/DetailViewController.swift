207e294f8d48dbbd9177dde3b8215b3d0bb92120
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-06-07 22:56:32 +0100

    + TextViewConstraints

--- iCookPad/Controllers/Detail Recipe/DetailViewController.swift
+++ iCookPad/Controllers/Detail Recipe/DetailViewController.swift
@@ -45,8 +45,10 @@ class DetailViewController: UIViewController {
         label.font = .systemFont(ofSize: 20, weight: .regular)
         label.textColor = UIColor.LabelColors.secondLabelColor
         label.isScrollEnabled = true
+        label.text = "xxxx xx xxxx x xx xx xxxxxx x xxx x x xxxxxxx x xx xx xxxxxxxxx xx xxxx x xx xx xxxxxx x xxx x x xxxxxxx x xx xx xxxxxxxxx xx xxxx x xx xx xxxxxx x xxx x x xxxxxxx x xx xx xxxxxxxxx xx xxxx x xx xx xxxxxx x xxx x x xxxxxxx x xx xx xxxxxxxxx xx xxxx x xx xx xxxxxx x xxx x x xxxxxxx x xx xx xxxxxxxxx xx xxxx x xx xx xxxxxx x xxx x x xxxxxxx x xx xx xxxxxxxxx xx xxxx x xx xx xxxxxx x xxx x x xxxxxxx x xx xx xxxxxxxxx xx xxxx x xx xx xxxxxx x xxx x x xxxxxxx x xx xx xxxxx"
         label.isEditable = false
         label.textAlignment = .center
+        label.backgroundColor = .gray
         return label
     }()
     
@@ -65,6 +67,8 @@ class DetailViewController: UIViewController {
     private func setupSubviews() {
         view.addSubview(scrollView)
         scrollView.addSubview(titleLabel)
+        scrollView.addSubview(previewImage)
+        scrollView.addSubview(recipdeDesc)
     }
     
     private func setupConstraints() {
@@ -78,9 +82,20 @@ class DetailViewController: UIViewController {
             titleLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             titleLabel.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -300),
             titleLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 30),
-            titleLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 60)
+            titleLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 60),
             
             // PreviewImage
+            previewImage.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 0.45),
+            previewImage.heightAnchor.constraint(equalTo: previewImage.widthAnchor, multiplier: 0.85),
+            previewImage.leftAnchor.constraint(equalTo: scrollView.leftAnchor, constant: 30),
+            previewImage.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 40),
+            
+            // Description
+            recipdeDesc.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 0.45),
+            recipdeDesc.heightAnchor.constraint(equalTo: recipdeDesc.widthAnchor, multiplier: 0.85),
+            recipdeDesc.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: -30),
+            recipdeDesc.topAnchor.constraint(equalTo: previewImage.topAnchor),
+            recipdeDesc.centerYAnchor.constraint(equalTo: previewImage.centerYAnchor)
         ]
         NSLayoutConstraint.activate(constraints)
     }
