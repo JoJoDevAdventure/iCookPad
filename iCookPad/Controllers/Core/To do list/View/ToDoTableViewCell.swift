684131f43b5b5e030e61b0fe8aba571618daf459
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-06-12 00:34:02 +0100

    + Validation / Cancel Buttons

--- iCookPad/Controllers/Core/To do list/View/ToDoTableViewCell.swift
+++ iCookPad/Controllers/Core/To do list/View/ToDoTableViewCell.swift
@@ -25,14 +25,27 @@ class ToDoTableViewCell: UITableViewCell {
         return image
     }()
     
-    private lazy var seeCellImage: UIImageView = {
+    private lazy var cancelRecipe: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
-        image.image = UIImage(systemName: "chevron.right.circle")
+        image.image = UIImage(systemName: "x.circle")
         image.backgroundColor = .clear
-        image.tintColor = .lightGray
+        image.contentMode = .scaleAspectFit
+        image.tintColor = UIColor.Buttons.redButton
         return image
     }()
+    
+    private lazy var validateRecipe: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(systemName: "checkmark.circle")
+        image.backgroundColor = .clear
+        image.contentMode = .scaleAspectFit
+        image.tintColor = UIColor.Buttons.greenButton
+        return image
+    }()
+    
+    private lazy var title
     
     // MARK: - View Model
     
@@ -54,6 +67,8 @@ class ToDoTableViewCell: UITableViewCell {
     private func setupSubviews() {
         addSubview(container)
         container.addSubview(previewImage)
+        container.addSubview(cancelRecipe)
+        container.addSubview(validateRecipe)
     }
     
     private func setupConstraints() {
@@ -66,7 +81,19 @@ class ToDoTableViewCell: UITableViewCell {
             previewImage.leftAnchor.constraint(equalTo: container.leftAnchor, constant: 10),
             previewImage.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -10),
             previewImage.topAnchor.constraint(equalTo: container.topAnchor, constant: 10),
-            previewImage.widthAnchor.constraint(equalTo: previewImage.heightAnchor, multiplier: 1.2)
+            previewImage.widthAnchor.constraint(equalTo: previewImage.heightAnchor, multiplier: 1.2),
+            
+            cancelRecipe.widthAnchor.constraint(equalTo: container.heightAnchor, multiplier: 0.3),
+            cancelRecipe.heightAnchor.constraint(equalTo: cancelRecipe.widthAnchor),
+            cancelRecipe.rightAnchor.constraint(equalTo: container.rightAnchor, constant: -15),
+            cancelRecipe.centerYAnchor.constraint(equalTo: container.centerYAnchor, constant: 35),
+            
+            validateRecipe.widthAnchor.constraint(equalTo: container.heightAnchor, multiplier: 0.3),
+            validateRecipe.heightAnchor.constraint(equalTo: validateRecipe.widthAnchor),
+            validateRecipe.rightAnchor.constraint(equalTo: container.rightAnchor, constant: -15),
+            validateRecipe.centerYAnchor.constraint(equalTo: container.centerYAnchor, constant: -35),
+            
+            
         ]
         NSLayoutConstraint.activate(constraints)
     }
