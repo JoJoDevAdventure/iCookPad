ca96e24dd513f489a1df71615e407b31a6e20105
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-06-11 23:41:13 +0100

    + Container for cell UI

--- iCookPad/Controllers/Core/To do list/View/ToDoTableViewCell.swift
+++ iCookPad/Controllers/Core/To do list/View/ToDoTableViewCell.swift
@@ -10,14 +10,29 @@ import UIKit
 class ToDoTableViewCell: UITableViewCell {
     
     // MARK: - Properties
+    private lazy var container: ItemsContainerView = {
+        let container = ItemsContainerView()
+        container.configure()
+        container.layer.cornerRadius = 30
+        return container
+    }()
+    
     private lazy var previewImage: PreviewImageView = {
         let image = PreviewImageView()
         image.configure()
-        image.layer.cornerRadius = 15
+        image.layer.cornerRadius = 23
         image.backgroundColor = .lightGray
         return image
     }()
     
+    private lazy var seeCellImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(systemName: "chevron.right.circle")
+        image.backgroundColor = .clear
+        image.tintColor = .lightGray
+        return image
+    }()
     
     // MARK: - View Model
     
@@ -25,6 +40,7 @@ class ToDoTableViewCell: UITableViewCell {
     // MARK: - Life cycle
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
         setupSubviews()
         setupConstraints()
     }
@@ -36,15 +52,21 @@ class ToDoTableViewCell: UITableViewCell {
     // MARK: - Set up
     
     private func setupSubviews() {
-        addSubview(previewImage)
+        addSubview(container)
+        container.addSubview(previewImage)
     }
     
     private func setupConstraints() {
         let constraints = [
-            previewImage.leftAnchor.constraint(equalTo: leftAnchor, constant: 20),
-            previewImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
-            previewImage.topAnchor.constraint(equalTo: topAnchor, constant: 10),
-            previewImage.widthAnchor.constraint(equalTo: previewImage.heightAnchor, multiplier: 1.1)
+            container.leftAnchor.constraint(equalTo: leftAnchor, constant: 15),
+            container.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            container.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            container.rightAnchor.constraint(equalTo: rightAnchor, constant: -15),
+            
+            previewImage.leftAnchor.constraint(equalTo: container.leftAnchor, constant: 10),
+            previewImage.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -10),
+            previewImage.topAnchor.constraint(equalTo: container.topAnchor, constant: 10),
+            previewImage.widthAnchor.constraint(equalTo: previewImage.heightAnchor, multiplier: 1.2)
         ]
         NSLayoutConstraint.activate(constraints)
     }
