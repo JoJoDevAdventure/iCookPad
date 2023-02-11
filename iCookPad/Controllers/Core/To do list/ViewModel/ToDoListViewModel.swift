b153b09abcf3a94de2f1654d867f0a56f67e497a
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-02-11 16:31:01 +0100

    + ToDo VM

--- iCookPad/Controllers/Core/To do list/ViewModel/ToDoListViewModel.swift
+++ iCookPad/Controllers/Core/To do list/ViewModel/ToDoListViewModel.swift
@@ -6,11 +6,44 @@
 //
 
 import Foundation
+protocol ToDoListViewModelOutput {
+    func didLoadLocalRecipes(recipes: [RecipeItem])
+    func failedToLoadRecipes(error: Error)
+    func didDeleteRecipe()
+    func failedToDeleteRecipe(error: Error)
+}
 
 class ToDoListViewModel {
     
+    public var output: ToDoListViewModelOutput?
+    
     init() {
-        
+        //TODO: Implement integration for LocalDataManager
     }
+    
+    public func fetchLocalStorageForDownload() {
+        DataPersistantManager.shared.fetchingTitlesFromDatabase { [weak self] result in
+            switch result {
+            case .success(let recipes):
+                DispatchQueue.main.async {
+                    self?.output?.didLoadLocalRecipes(recipes: recipes)
+                }
+            case.failure(let error) :
+                self?.output?.failedToLoadRecipes(error: error)
+            }
+        }
+    }
+    
+    public func deleteRecipe(recipe: RecipeItem) {
+        DataPersistantManager.shared.deleteRecipeWith(model: recipe) {[weak self] results in
+            switch results {
+            case .success():
+                self?.output?.didDeleteRecipe()
+            case .failure(let error):
+                self?.output?.failedToDeleteRecipe(error: error)
+            }
+        }
+    }
+    
     
 }
