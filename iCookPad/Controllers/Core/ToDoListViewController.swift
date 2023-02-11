f84b7b0e383b3e84e64ff169affc28c52c4995cf
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2023-02-11 16:30:16 +0100

    + ToDos UI

--- iCookPad/Controllers/Core/ToDoListViewController.swift
+++ iCookPad/Controllers/Core/ToDoListViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 class ToDoListViewController: UIViewController {
 
     // MARK: - Properties
+    var recipes: [RecipeItem] = []
     
     private lazy var tableView: UITableView = {
         let table = UITableView()
         table.registerCell(ToDoTableViewCell.self)
         table.backgroundColor = UIColor.BackgroundColors.background
         table.separatorColor = .clear
+        table.isUserInteractionEnabled = true
         return table
     }()
     
@@ -25,6 +27,7 @@ class ToDoListViewController: UIViewController {
     init(viewModel: ToDoListViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+        viewModel.output = self
     }
 
     required init?(coder: NSCoder) {
@@ -38,6 +41,12 @@ class ToDoListViewController: UIViewController {
         setupConstraints()
         setupNavBar()
         setupTableView()
+        view.isUserInteractionEnabled = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        fetchData()
     }
 
     // MARK: - Set up
@@ -45,6 +54,31 @@ class ToDoListViewController: UIViewController {
     private func setupNavBar() {
         title = "ToDo List 📝"
         navigationController?.largeTitle()
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(didPressDelete))
+    }
+    
+    @objc private func didPressDelete() {
+        AlertsManager.shared.verificationAlert(with: self, sure: "Do you want to delete all your recipes ?") { yes in
+            if yes {
+                self.deleteAllRecipes()
+            } else {
+                return
+            }
+        }
+    }
+    
+    
+    private func deleteAllRecipes() {
+        DataPersistantManager.shared.deleteAllRecipes(items: recipes) { results in
+            switch results {
+            case .success(let success):
+                self.recipes = []
+                self.tableView.reloadData()
+               return
+            case .failure(let failure):
+                return
+            }
+        }
     }
     
     private func setupSubviews() {
@@ -65,7 +99,9 @@ class ToDoListViewController: UIViewController {
     }
     
     // MARK: - Functions
-    
+    private func fetchData() {
+        viewModel.fetchLocalStorageForDownload()
+    }
     
     // MARK: - Network Manager calls
     
@@ -75,11 +111,14 @@ class ToDoListViewController: UIViewController {
 extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.recipes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(for: ToDoTableViewCell.self, for: indexPath)
+        let recipe = recipes[indexPath.row]
+        cell.configure(recipe: recipe)
+        cell.delegate = self
         return cell
     }
     
@@ -87,4 +126,40 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
         return 200
     }
 
+}
+
+extension ToDoListViewController: ToDoListViewModelOutput {
+    func didDeleteRecipe() {
+        fetchData()
+    }
+    
+    func failedToDeleteRecipe(error: Error) {
+        AlertsManager.shared.errorAlert(with: self, error: error.localizedDescription)
+    }
+    
+    func didLoadLocalRecipes(recipes: [RecipeItem]) {
+        self.recipes = recipes
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func failedToLoadRecipes(error: Error) {
+        AlertsManager.shared.errorAlert(with: self, error: error.localizedDescription)
+    }
+    
+}
+
+extension ToDoListViewController: ToDoTableViewCellDelegate {
+    
+    func didPressDeleteRecipe(recipe: RecipeItem) {
+        AlertsManager.shared.verificationAlert(with: self, sure: "Are you sure to delete this recipe from your ToDos?") { yes in
+            yes ? self.viewModel.deleteRecipe(recipe: recipe) : ()
+        }
+    }
+    
+    func didPressArchieveRecipe(recipe: RecipeItem) {
+        //TODO: Archieve
+    }
+    
 }
